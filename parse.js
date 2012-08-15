2986ab2b4b9a399ce318859fd18532294515a36e
Fabrice Leal vmUbuntu <fabrice.leal.ch@gmail.com>
2012-08-15 07:44:09 -0700

    prints raw json

--- parse.js
+++ parse.js
@@ -3,9 +3,21 @@
 with(require('./pokerstars.js').parser) {
 
 	require('fs').readFile(process.argv[2], "utf-8", function(err, data){
-		if(err) throw err;
+		if(err){
+			console.error('Error reading file ' + process.argv[2]);
+			console.error(err);
 
-		console.log(JSON.stringify(parse(data), null, 3));
+			process.exit(1);
+		}
+
+		try{
+			console.log(JSON.stringify(parse(data))); // , null, 3
+		} catch(e){
+			console.error('Error processing file ' + process.argv[2]);
+			console.error(e);
+
+			process.exit(2);
+		}
 		
 	});
 
